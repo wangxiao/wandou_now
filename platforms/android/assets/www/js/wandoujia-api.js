8ed13a798469e674e946b09c9b7a3346600372f0
尼奥 <1988wangxiao@gmail.com>
2014-02-06 21:49:03 +0800

    [feature] Change file name.

---
+++ platforms/android/assets/www/js/wandoujia-api.js
@@ -0,0 +1,62 @@
+// phonegap API
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+// 豌豆荚的 API
+var wandoujiaApi = {
+    dianping: function() {
+        return $.ajax({
+            type: 'get',
+            url: 'http://192.168.108.151:8089/shop',
+            async: false,
+            contentType: 'application/json',
+            dataType: 'jsonp',
+            timeout: 10000
+        }).done(function(list) {
+        }).fail(function(xhr, status, error) {
+        });
+    }
+};
+
+// 基础方法
+var baseApi = {
+    openApp: function(url) {
+        window.plugins.webintent.startActivity({
+            action: window.plugins.webintent.ACTION_VIEW,
+            url: url
+            // url: 'dianping://shopinfo?id=4175436'
+        }, function() {
+        }, function() {
+        });
+    }
+};
