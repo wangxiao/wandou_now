e5fd916974e43e736f0771eeabaafd48b41b7fd5
尼奥 <1988wangxiao@gmail.com>
2014-02-06 21:21:57 +0800

    [feature] Remove all ignore file.

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Applications/XAMPP/xamppfiles/htdocs/wandoujia/wandou_now/platforms/android/bin/AndroidManifest.xml : \
-/Applications/XAMPP/xamppfiles/htdocs/wandoujia/wandou_now/platforms/android/AndroidManifest.xml \
